a443182f456600a468ca8504876f8be9d0707723
DivyakshChachan <divyaksh3@gmail.com>
2026-07-19 21:13:15 +0530

    first commit

---
+++ src/main/java/com/divyaksh/cap/CodingAssessmentPlatformApplication.java
@@ -0,0 +1,13 @@
+package com.divyaksh.cap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodingAssessmentPlatformApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CodingAssessmentPlatformApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/divyaksh/cap/CodingAssessmentPlatformApplicationTests.java
@@ -0,0 +1,13 @@
+package com.divyaksh.cap;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodingAssessmentPlatformApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
